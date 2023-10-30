d18628445ef8a0c9bc4d4254ecae0ad9856b9f73
feizzer <feizzer@163.com>
2023-10-30 21:20:50 +0800

    revert

--- PEParse/main.c
+++ PEParse/main.c
@@ -7,10 +7,11 @@
 int main() {
     char* buf = NULL;
     unsigned long size = 0;
-   /* if (!LoadFile("E:\\messageBox32.exe", (void**)&buf, &size)) {
+    if (!LoadFile("E:\\messageBox32.exe", (void**)&buf, &size)) {
+        printf("failed");
         return 1;
     }
-    printf("%d",  ClassifyPE(buf, size));*/
+    printf("%d",  ClassifyPE(buf, size));
 
     return 0;
 }
