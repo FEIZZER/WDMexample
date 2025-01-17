5efca1b5fc2b3d08d5ff5fa536d6011d7721187f
feizzer <feizzer@163.com>
2025-01-17 18:20:36 +0800

    :construction:safelist

---
+++ myutils/include/ez/process_helper.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+namespace ez {
+
+	inline unsigned int execute_command_line(const std::string& cmd, const std::string&)
+	{
+
+	}
+
+}
---
+++ myutils/include/ez/safelist.hpp
@@ -0,0 +1,83 @@
+#pragma once
+#include <list>
+#include <xmemory>
+#include <mutex>
+#include <shared_mutex>
+
+namespace ez {
+
+template <class _Ty, class _Alloc = std::allocator<_Ty>>
+class list
+{
+public:
+	using read_lock = std::shared_lock<std::shared_mutex>;
+	using write_lock = std::unique_lock<std::shared_mutex>;
+
+	list() = default;
+	~list() {
+		write_lock lock(shared_mutex_);
+		list_.empty();
+	}
+
+	bool push_back(const _Ty& item) {
+		write_lock lock(shared_mutex_);
+		list_.push_back(item);
+		return true;
+	}
+
+	bool push_back(_Ty&& item) {
+		write_lock lock(shared_mutex_);
+		list_.push_back(std::move(item));
+		return true;
+	}
+
+	bool push_front(const _Ty& item) {
+		write_lock lock(shared_mutex_);
+		list_.push_front(item);
+		return true;
+	}
+
+	bool push_front(_Ty&& item) {
+		write_lock lock(shared_mutex_);
+		list_.push_front(std::move(item));
+		return true;
+	}
+
+
+	bool pop_back(_Ty& item) {
+		write_lock lock(shared_mutex_);
+		if (list_.empty()) {
+			return false;
+		}
+		item = list_.back();
+		list_.pop_back();
+		return true;
+	}
+
+	bool pop_front(_Ty& item){
+		write_lock lock(shared_mutex_);
+		if (list_.empty()) {
+			return false;
+		}
+		item = std::move(list_.front());
+		list_.pop_front();
+		return true;
+	}
+
+	bool empty(){
+		read_lock lock(shared_mutex_);
+		return list_.empty();
+	}
+
+	std::size_t size() {
+		read_lock lock(shared_mutex_);
+		return list_.size();
+	}
+
+private:
+	std::list<_Ty, _Alloc>	list_;
+	std::shared_mutex		shared_mutex_;
+};
+
+
+}
