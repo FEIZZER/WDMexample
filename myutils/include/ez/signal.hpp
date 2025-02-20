f04a7c90f2559e5df980b0c180fb437fc3b9a954
feizzer <feizzer@163.com>
2025-02-20 17:38:13 +0800

    fix bug

--- myutils/include/ez/signal.hpp
+++ myutils/include/ez/signal.hpp
@@ -8,45 +8,69 @@ namespace ez {
 class signal
 {
 public:
-	signal() = default;
+	signal()
+	{
+		re_ = true;
+		sg_ = false;
+	}
+
 	~signal() = default;
 
 	void wait(){
 		std::unique_lock<std::mutex> lock(mu_);
-		cv_.wait(lock);
+		while (!sg_) {
+			cv_.wait(lock);
+		}
+		if(re_)
+			sg_ = false;
 	}
 
-	template<class _Predicate>
-	void wait(_Predicate pred) {
-		std::unique_lock<std::mutex> lock(mu_);
-		cv_.wait(lock, pred);
-	}
 
 	template <class _Rep, class _Period>
 	bool wait_for(const std::chrono::duration<_Rep, _Period>& wait_time) {
+		bool no_timeout;
+		auto dead_line = std::chrono::system_clock::now() + wait_time;
 		std::unique_lock<std::mutex> lock(mu_);
-		auto status = cv_.wait_for(lock, wait_time);
-		return status == std::cv_status::no_timeout;
-	}
-
-	template <class _Rep, class _Period, class _Predicate>
-	bool wait_for(const std::chrono::duration<_Rep, _Period>& wait_time, _Predicate pred) {
-		std::unique_lock<std::mutex> lock(mu_);
-		return cv_.wait_for(lock, wait_time, pred);
+		while (!sg_) {
+			no_timeout = cv_.wait_until(lock, dead_line) == std::cv_status::no_timeout;
+			if (!no_timeout) {
+				break;
+			}
+		}
+		if (no_timeout && re_)  
+			sg_ = false;
+		
+		return no_timeout;
 	}
 
 	void notify_all() {
+		{
+			std::lock_guard<std::mutex> lock(mu_);
+			sg_ = true;
+			re_ = false;
+		}
 		cv_.notify_all();
 	}
 
 	void notify_one(){
+		{
+			std::lock_guard<std::mutex> lock(mu_);
+			sg_ = true;
+		}
 		cv_.notify_one();
+	}
+
+	void reset() {
+		std::lock_guard<std::mutex> lock(mu_);
+		sg_ = false;
+		re_ = true;
 	}
 
 
 private:
+	bool					re_;
 	std::mutex				mu_;
-	std::atomic_bool		sig_;
+	std::atomic_bool		sg_;
 	std::condition_variable cv_;
 };
 }
